5ef8a13d2cd653a2d9b26c343e355fd5e0d769bd
Gagan Gowda <gagangowda26@gmail.com>
2022-12-30 15:14:17 -0800

    fixed auto-reload problem arising from forms

--- scripts/script.js
+++ scripts/script.js
@@ -11,3 +11,31 @@ const operate = function(op, a, b) {
         case '/' : return divide(a, b);
     }
 }
+
+displayField = document.querySelector('#display');
+clearBtn = document.querySelector('#op-clear');
+opButtons = document.querySelectorAll('.operations button');
+displayField.textContent = 0;
+let exp = '';
+
+numButtons = document.querySelectorAll('.numbers button');
+numButtons.forEach(numButton => {
+    numButton.addEventListener('click', (e) => {
+        exp += numButton.textContent;
+        displayField.textContent = exp;
+    });
+});
+
+clearBtn.addEventListener('click', (e) => {
+    console.log(displayField.textContent);
+    displayField.textContent = 0;
+})
+
+opButtons.forEach(opbtn => {
+    opbtn.addEventListener('click', (e) => {
+        exp += opbtn.textContent;
+        displayField.textContent = exp;
+    })
+})
+
+
